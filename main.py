97eb4bd1b985668272ab994f43c4b2b1a26f0eb2
Luca Vecchio <luca1.vecchio@mail.polimi.it>
2021-06-02 00:12:11 +0200

    feat: find and print dataset

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from parse import *
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def print_coords(dataset):
+    missing = []
+    for i in range(0, 10, 2):
+        for j in range(0, 10, 2):
+            if (i, j) not in dataset.keys():
+                missing.append((i, j))
+                continue
+            print('{}, {}: {}'.format(i, j, dataset[(i, j)]))
+
+    if len(missing) != 0:
+        print('Missing values:')
+        for item in missing:
+            print(item)
+    else:
+        print('There is no missing value!')
+
+
+def get_dataset(filename):
+    dataset_local = {}
+    with open(filename) as f:
+        for line in f.readlines():
+            result = parse('{},{} {}|{}|{}|{}|{}|{}', line)
+
+            if result is not None:
+                x, y = float(result[0]), float(result[1])
+                rssi_str = [result[i] for i in range(2, 7)]
+                dataset_local[(x, y)] = rssi_str
+            else:
+                print('Parser error: \n' + line)
+
+    return dataset_local
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    dataset = get_dataset('dataset.dat')
+    print_coords(dataset)
